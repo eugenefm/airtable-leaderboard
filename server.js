01d33393cea70128bfd8c25c35ffd8b3fdb7eb17
Eugene Michasiw <eugene@michasiw.com>
2020-02-29 11:54:55 -0500

    fix serving static files

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 // API route to list rows from Airtable:
 
 const connection = require("./database");
